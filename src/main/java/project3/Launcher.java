87f62aed2fb76af00699c6692d37453bcd17b540
Joe <jguanine@gmail.com>
2026-03-23 03:27:37 -0400

    preliminary changes. A few tabs in the view, and frontend is in controller

--- src/main/java/project3/Launcher.java
+++ src/main/java/project3/Launcher.java
@@ -1,9 +1,7 @@
 package project3;
 
-import javafx.application.Application;
-
 public class Launcher {
     public static void main(String[] args) {
-        Application.launch(Main.class, args);
+        Main.main(args);
     }
 }
--- src/main/java/project3/Main.java
+++ src/main/java/project3/Main.java
@@ -5,15 +5,19 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-import java.io.IOException;
-
 public class Main extends Application {
+
     @Override
-    public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+    public void start(Stage stage) throws Exception {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("view.fxml"));
+        Scene scene = new Scene(loader.load());
+
+        stage.setTitle("Student Registration System");
         stage.setScene(scene);
         stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
     }
 }
